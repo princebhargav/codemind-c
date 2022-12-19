14ff72b4bab73ea8bca1c0f4c1019ff7b8d463c9
princebhargav <119729726+princebhargav@users.noreply.github.com>
2022-12-19 20:54:56 +0530

    #include<stdio.h> int main() {     int x,y,z,a,b;     scanf("%d%d%d",&x,&y,&z);     a=((x*5)+(y*10));     b=a/z;     if(a>z)     {         printf("%d",b);
        }
        else
        {
            printf("%d",b);
            
        }
    }

---
+++ Romeo_and_Juliet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z,a,b;
+    scanf("%d%d%d",&x,&y,&z);
+    a=((x*5)+(y*10));
+    b=a/z;
+    if(a>z)
+    {
+        printf("%d",b);
+        
+    }
+    else
+    {
+        printf("%d",b);
+        
+    }
+}
