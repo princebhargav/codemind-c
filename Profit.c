dbbc590eb978c1d0fb943e23cbb311576a9ed0dc
princebhargav <119729726+princebhargav@users.noreply.github.com>
2022-12-30 15:51:46 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d%d",&n,&i);     if(i>n)     {         printf("Profit");
        }
        else
        {
            printf("Loss");
            
        }
    }

---
+++ Profit.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d%d",&n,&i);
+    if(i>n)
+    {
+        printf("Profit");
+        
+    }
+    else
+    {
+        printf("Loss");
+        
+    }
+}
